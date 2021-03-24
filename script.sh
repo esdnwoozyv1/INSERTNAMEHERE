84b2e1a8e2b0dc82b01e911148668adec302bb01
esdnwoozyv1 <72991938+esdnwoozyv1@users.noreply.github.com>
2021-03-24 16:16:14 -0400

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# This script automates initial enumeration.
+#
+#
+#
+printf "Welcome to ###(InsertNameHere)###\n"
+printf  "###(InsertNameHere)### is an automation script that performs Initial Enumeration..."
+read -p "Please Enter an IP Address or Range: " ip
+printf "\nScanning $ip commencing...\n"
+nmap -A $ip >> hosts.txt
+printf  "\nScanning Complete on $ip\n"
+printf "\nWould you like to proceed with vulnerabiltiy scanning?\n"
+read -p 'yes or no: ' yn
+        case $yn in
+                [Yy]* ) nikto -host http://$ip >> nikto_vuln_$ip.txt;
+                [Nn]*  exit;;
+                * ) echo "Exiting...";;
+        esac
+
+#gobuster dir -u http://$ip -w /usr/share/wordlists/dirb/common.txt
+#nikto -host http://$ip
