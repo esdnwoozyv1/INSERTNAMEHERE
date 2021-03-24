afce1f832ed2eb054f24294d9ba14cc77267f1fe
esdnwoozyv1 <72991938+esdnwoozyv1@users.noreply.github.com>
2021-03-24 17:58:40 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,20 +3,28 @@
 # This script automates initial enumeration.
 #
 #
-#
-printf '\e[1;34m\e[m' "Welcome to ###(InsertNameHere)###\n"
-printf  "###(InsertNameHere)### is an automation script that performs Initial Enumeration..."
+figlet FragRecon
+printf "\n\e[1;31mWelcome to ###(InsertNameHere)###\e[0m\n"
+printf  "\n###(InsertNameHere)### is an automation script that performs Initial Enumeration...\n"
+printf "\n"
 read -p "Please Enter an IP Address or Range: " ip
 printf "\nScanning $ip commencing...\n"
-nmap -A $ip >> hosts.txt
+nmap -A $ip > scan_$ip.txt
 printf  "\nScanning Complete on $ip\n"
 printf "\nWould you like to proceed with vulnerabiltiy scanning?\n"
-read -p 'yes or no: ' yn
-        case $yn in
-                [Yy]* ) nikto -host http://$ip >> nikto_vuln_$ip.txt;
-                [Nn]*  exit;;
-                * ) echo "Exiting...";;
-        esac
+read -p 'yes or no: ' answer
+        if [[ $answer = y ]] ; then
+        echo "Vulnerability Scan Initiated..."
+        nikto -host $ip >> scan_$ip.txt
+        elif [[ $answer = n ]] ;
+        echo "Exiting FragRecon..."
+        exit
+
+#        case $yn in
+#                [Yy]* ) nikto -host http://$ip >> scan_$ip.txt;
+#               [Nn]*  exit;;
+#              * ) echo "Exiting...";
+#     esac
 
 #gobuster dir -u http://$ip -w /usr/share/wordlists/dirb/common.txt
 #nikto -host http://$ip
